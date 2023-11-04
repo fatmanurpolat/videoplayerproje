d0d03e32dea1054a8b31eb002abdcff3bf325238
Fatmanur Polat <82556567+fatmanurpolat@users.noreply.github.com>
2023-11-04 21:48:34 +0300

    r

--- lib/auth/register/register_view_model.dart
+++ lib/auth/register/register_view_model.dart
@@ -8,28 +8,23 @@ class RegisterViewModel {
   TextEditingController confirmTextController = TextEditingController();
 
   Future<void> signUp(BuildContext context) async {
-    // Kullanıcı adı, şifre ve şifre onayı değerlerini alın
     String username = userNameTextController.text.trim();
     String password = passwordTextController.text.trim();
     String confirmPassword = confirmTextController.text.trim();
 
-    // Şifreleri kontrol edin
     if (password == confirmPassword) {
       try {
-        // Firebase ile kullanıcı kaydı yapın
         await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: username,
           password: password,
         );
 
-        // Kullanıcı başarıyla kaydedildiyse, ana sayfaya yönlendirin
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
             builder: (context) => HomePage(),
           ),
         );
       } catch (e) {
-        // Kullanıcı kaydı sırasında bir hata oluşursa, hatayı gösterin
         showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -49,7 +44,6 @@ class RegisterViewModel {
         );
       }
     } else {
-      // Şifreler uyuşmuyorsa kullanıcıya bilgi verin
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text("Şifreler uyuşmuyor!"),
       ));
